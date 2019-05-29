ffe7467f7ab3d7b6972045f4573367ea5a2c1c0b
Alberto <albertomaronezio@hotmai.com>
2019-05-29 16:35:57 -0300

    Exercicio_Parte2_13

---
+++ Exercicio_Parte2_13/Exercicio_Parte2_13/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Parte2_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int primeiroNumero = 0;
+            int segundoNumero = 0;
+            int terceiroNumero = 0;
+            List<int> listaDecrescente = new List<int>();
+
+            Console.Write("Digite o Primeiro Número: ");
+            primeiroNumero = int.Parse(Console.ReadLine());
+            listaDecrescente.Add(primeiroNumero);
+
+            Console.Write("Digite o Segundo Número: ");
+            segundoNumero = int.Parse(Console.ReadLine());
+            listaDecrescente.Add(segundoNumero);
+
+            Console.Write("Digite o Terceiro Número: ");
+            terceiroNumero = int.Parse(Console.ReadLine());
+            listaDecrescente.Add(terceiroNumero);
+
+            listaDecrescente.Sort();
+
+            foreach (int decrescente in listaDecrescente)
+            {
+                Console.WriteLine(decrescente);
+            }
+        }
+    }
+}
